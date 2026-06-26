27fa36c360de17a032cd8f9c0dae34ff92844347
Lucian <90592237+lungDa@users.noreply.github.com>
2026-06-26 11:56:59 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -436,7 +436,7 @@ class ViewComponents:
         if st.session_state.get("auth_user"):
             return True
 
-        st.title("🔐 鋒霈工程部 平台登入")
+        st.title("🔐 鋒霈  工程部   平台登入")
         st.caption("請使用人員帳號(打工號數字)與密碼登入。預設密碼為 0000，首次登入後請立即修改。")
 
         with st.container(border=True):
